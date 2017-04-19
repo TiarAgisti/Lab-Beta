004d8a49ad7e0718eed2cfc68452e03f40751c20
Tiar Agisti <tiar.agisti@gmail.com>
2017-04-19 21:02:52 +0700

    add AccessModel & AccountBankModel

---
+++ Lab-Beta/Lab-Beta.Models/Master/AccessModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Beta.Models.Master
+{
+    public class AccessModel
+    {
+        public int Id { get; set; }
+        public int GroupUserId { get; set; }
+        public string NameOption { get; set; }
+        public string Controller { get; set; }
+        public string Action { get; set; }
+        public string ImageClass { get; set; }
+        public bool Status { get; set; }
+        public bool isParent { get; set; }
+        public int parentId { get; set; }
+    }
+}
