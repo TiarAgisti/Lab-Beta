005f9f2f7fc077586dae67feec37847eeca89513
Tiar Agisti <tiar.agisti@gmail.com>
2017-04-19 21:31:12 +0700

    add Job & Location Model

---
+++ Lab-Beta/Lab-Beta.Models/Master/LocationModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Beta.Models.Master
+{
+    public class LocationModel
+    {
+        public int Id { get; set; }
+        public string CodeLocation { get; set; }
+        public string Name { get; set; }
+        public string Province { get; set; }
+        public Int16 City { get; set; }
+        public Int16 District { get; set; }
+        public Int16 Locality { get; set; }
+    }
+}
