6f163aa63fb0559fdc70d04e5a4e3fdc94a76c08
Tiar Agisti <tiar.agisti@gmail.com>
2017-04-19 22:21:04 +0700

    add SenderModel

---
+++ Lab-Beta/Lab-Beta.Models/Master/SenderModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Beta.Models.Master
+{
+    public class SenderModel:CoreModel
+    {
+        public string SenderName { get; set; }
+        public string ContactName { get; set; }
+        public string Address { get; set; }
+
+        public int Location_ID { get; set; }
+        public string TelpHome { get; set; }
+        public string TelpWork { get; set; }
+        public string Email { get; set; }
+
+        public double Discount { get; set; }
+        public int AccountBankID { get; set; }
+        public int MethodPaymentID { get; set; }
+
+        public string AccountNumber { get; set; }
+        public string AccountName { get; set; }
+    }
+}
