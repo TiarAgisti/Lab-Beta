11abb5e717b9dd7a722a99331fa89ad84880cf19
Tiar Agisti <tiar.agisti@gmail.com>
2017-04-19 20:57:38 +0700

    add CoreModel and Update BloodGroupModel

---
+++ Lab-Beta/Lab-Beta.Models/CoreModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Beta.Models
+{
+    public class CoreModel
+    {
+        public int Id { get; set; }
+        public Nullable<int> UserCreate { get; set; }
+        public Nullable<System.DateTime> DateCreate { get; set; }
+        public Nullable<int> UserModify { get; set; }
+        public Nullable<System.DateTime> DateModify { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
--- Lab-Beta/Lab-Beta.Models/Master/BloodGroupModel.cs
+++ Lab-Beta/Lab-Beta.Models/Master/BloodGroupModel.cs
@@ -6,16 +6,10 @@ using System.Threading.Tasks;
 
 namespace Lab_Beta.Models
 {
-    public class BloodGroupModel
+    public class BloodGroupModel:CoreModel
     {
-        public int Id { get; set; }
         public string Name { get; set; }
         public string Type { get; set; }
         public string Rhesus { get; set; }
-        public Nullable<int> UserCreate { get; set; }
-        public Nullable<System.DateTime> DateCreate { get; set; }
-        public Nullable<int> UserModify { get; set; }
-        public Nullable<System.DateTime> DateModify { get; set; }
-        public bool IsActive { get; set; }
     }
 }
