a0b7d655bb569da04d7a0bc33c3a5db3495348ed
Tiar Agisti <tiar.agisti@gmail.com>
2017-04-19 22:23:27 +0700

    add SpecialistModel

---
+++ Lab-Beta/Lab-Beta.Models/Master/SpecialistModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Beta.Models.Master
+{
+    public class SpecialistModel:CoreModel
+    {
+        public string CodeSpecialist { get; set; }
+        public string Description { get; set; }
+    }
+}
