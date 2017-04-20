439632c2654ba8850ab24ff1e91e6ff3cb92af06
hendrysan <hendry.priyatno@gmail.com>
2017-04-20 22:42:06 +0700

    BF contro

---
+++ Lab-Beta/Lab-Beta.BF/BloodGroupBF.cs
@@ -0,0 +1,29 @@
+﻿using Lab_Beta.EDM;
+using Lab_Beta.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Beta.BF
+{
+    public class BloodGroupBF
+    {
+        private LaboratoriesEntities db = null;
+
+        public List<BloodGroupModel> SelectComboBox()
+        {
+            db = new LaboratoriesEntities();
+
+            List<BloodGroupModel> data = db.BloodGroups.Where(i => i.IsActive == true)
+                                        .Select(i => new BloodGroupModel
+                                           {
+                                               Id = i.Id,
+                                               Name = i.Name
+                                           }).ToList();
+
+            return data;
+        }
+    }
+}
---
+++ Lab-Beta/Lab-Beta.Controller/BloodGroupController.cs
@@ -0,0 +1,22 @@
+﻿using Lab_Beta.BF;
+using Lab_Beta.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Beta.Controller
+{
+    public class BloodGroupController
+    {
+        private BloodGroupBF bloodBF = null;
+
+        public List<BloodGroupModel> ListComboBox()
+        {
+            bloodBF = new BloodGroupBF();
+            return bloodBF.SelectComboBox();
+        }
+
+    }
+}
